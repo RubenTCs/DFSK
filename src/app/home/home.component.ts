cce1c6247cc46bef2db0c2306344b1ea19e14ffb
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2023-11-22 22:50:44 +0700

    Added home & navbar component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
