45fe99cf5b20bf86312416d76ef60abf3df29e90
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2023-11-24 15:40:01 +0700

    Simple Navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from "./essentials/navbar/navbar.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomeComponent, NavbarComponent]
 })
 export class AppComponent {
-  title = 'DFSK ng-awi';
+  title = 'DongFeng ng-awi';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarComponent } from '../essentials/navbar/navbar.component';
 @Component({
   selector: 'app-home',
   standalone: true,
