12505a132baf2bb2d3fff745ecc77489c0c273ca
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2023-11-26 01:28:48 +0700

    basically done, need some adjustment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShowroomComponent } from './showroom/showroom.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
     {path: 'home', component:HomeComponent},
-    {path: 'showroom', component: ShowroomComponent}
+    {path: 'showroom', component: ShowroomComponent},
+    {path: 'about', component: AboutComponent}
 ];
